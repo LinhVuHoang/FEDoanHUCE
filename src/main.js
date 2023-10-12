7da9ebaec7e19654ae4e1473c8ea6b30ffb5d570
LinhVuHoang <redtonlinh@gmail.com>
2023-10-12 17:33:20 +0700

    update export excel

--- src/main.js
+++ src/main.js
@@ -19,7 +19,6 @@ import DefaultLayout from './layouts/Default.vue'
 import DashboardLayout from './layouts/Dashboard.vue'
 import DashboardRTLLayout from './layouts/DashboardRTL.vue'
 import router from './router'
-
 // import './plugins/click-away'
 
 import './scss/app.scss';
--- src/views/DSSV.vue
+++ src/views/DSSV.vue
@@ -4,7 +4,9 @@
       <label>Lớp: {{this.data[0].MaLopHoc}}</label><br>
       <label>Môn: {{this.data[0].TenMonHoc}}</label><br>
       <label>Sĩ số: {{this.data[0].SiSo}}</label>
-      <button @click="exportToExcel" style="float: right">Export to Excel</button>
+      <button type="button" class="download-btn" style="float: right" v-on:click="exportToExcel">
+        <a-icon type="file-excel" theme="filled"/>
+      </button>
       <div class="table-wrap">
         <table class="table table-bordered" style="--bs-table-bg: dodgerblue; border-right: black">
           <thead>
@@ -20,9 +22,9 @@
           </thead>
           <tbody>
           <template >
-              <tr v-for="(sv,index) in data">
+              <tr v-for="(sv) in data">
                 <td >
-                  <span style="color: blue">{{index+1}}</span>
+                  <span style="color: blue">{{sv.STT}}</span>
                 </td>
                 <td >
                   <span style="color: blue">{{sv.MaSinhVien}}</span>
@@ -63,6 +65,9 @@
 </template>
 <script>
 import TKBHocKyService from "@/service/TKBHocKyService";
+// import exportFromJSON from "export-from-json";
+import ExcelJS from "exceljs";
+import * as XLSX from 'xlsx'
 export default {
   data(){
     return{
@@ -71,7 +76,7 @@ export default {
       totalPages:undefined,
       TenPhong:undefined,
       new_data:[],
-      datahk:[]
+      datahk:[],
     }
   },
   created() {
@@ -85,6 +90,9 @@ export default {
             try{
               this.data=[]
               this.data =  rs.data.result.recordset;
+              for (let i = 0; i < this.data.length; i++) {
+                this.data[i].STT = i + 1; // Bắt đầu index từ 1 hoặc 0 tùy theo tùy chọn của bạn
+              }
               console.log(this.data)
              // this.totalRecords = rs.data.filtered;
             }catch (e){
@@ -94,38 +102,80 @@ export default {
           }
       )
     },
-    exportToExcel() {
-      // Your data to be exported
+    // downloadFile() {
+    //   const data = this.investorsList;
+    //   const fileName = "np-data";
+    //   const exportType = exportFromJSON.types.xls;
+    //
+    //   if (data) exportFromJSON({ data, fileName, exportType });
+    // },
+//     download(){
+//       const data = XLSX.utils.json_to_sheet(this.OrderProperties(this.customizeData(this.data)))
+//       const wb = XLSX.utils.book_new()
+//       // Tạo nội dung tùy chỉnh
+//       XLSX.utils.book_append_sheet(wb, data, 'DanhSachSinhVien')
+//       // Tạo một dòng tiêu đề
+//     //  const headerRow = ["Danh sách sinh vien", "Sĩ số"];
+//
+// // Thêm dòng tiêu đề vào worksheet
+//       //XLSX.utils.sheet_add_aoa(this.data[0].SiSo, [headerRow], { origin: 'A1' });
+//       XLSX.writeFile(wb,'Danh_sach_sinh_vien_'+this.data[0].MaLopHoc+'.xlsx')
+//     },
+    async exportToExcel() {
+      const workbook = new ExcelJS.Workbook();
+      const worksheet = workbook.addWorksheet("Danh sách sinh viên");
+      const studentData=this.OrderProperties(this.customizeData(this.data))
+      console.log(studentData)
+      const titleRow = worksheet.addRow(["DANH SÁCH SINH VIÊN LỚP HỌC PHẦN"]);
+      titleRow.font = { size: 16, bold: true }; // Đặt kiểu chữ to và in đậm
       const data = [
-        ['Name', 'Age'],
-        ['John', 30],
-        ['Jane', 28],
-        ['Doe', 35],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        ["Mã học phần:", this.data[0].MaLopHocPhan," "," ","Học kỳ:",this.data[0].TenDot],
+        ["Tên học phần:", this.data[0].TenMonHoc," "," ","Lớp học:",this.data[0].MaLopHoc],
+        ["Sĩ số:", this.data[0].SiSo],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        [" "," "," "," "," "," "," "," "],
+        ["STT","Mã sinh viên","Họ Đệm","Tên","Email","Số điện thoại","Ngày Sinh","Lớp quản lý"]
+        // Add more rows as needed
       ];
+      for (let i=0;i<studentData.length;i++){
+        const studentArray = Object.values(studentData[i])
+        data.push(studentArray)
+      }
 
-      // Create a new worksheet
-      const ws = XLSX.utils.aoa_to_sheet(data);
+      // Add the data to the worksheet
+      data.forEach((row) => {
+        worksheet.addRow(row);
+      });
 
-      // Create a new workbook
-      const wb = XLSX.utils.book_new();
+      // Create a Blob from the Excel workbook
+      const blob = await workbook.xlsx.writeBuffer();
 
-      // Add the worksheet to the workbook
-      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-
-      // Generate a blob from the workbook
-      const blob = XLSX.write(wb, { bookType: 'xlsx', type: 'blob' });
-
-      // Create a downloadable link
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = 'exported-data.xlsx';
-
-      // Trigger a click event on the link to start the download
+      // Create a Blob URL and trigger a download
+      const blobURL = URL.createObjectURL(new Blob([blob], { type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" }));
+      const a = document.createElement("a");
+      a.href = blobURL;
+      a.download = "DanhSachSinhVien" +this.data[0].MaLopHoc+".xlsx";
+      a.style.display = "none";
+      document.body.appendChild(a);
       a.click();
-
-      // Release the URL object
-      window.URL.revokeObjectURL(url);
+      document.body.removeChild(a);
+    },
+    customizeData(data){
+      return data.map((dssv)=>{
+        // eslint-disable-next-line no-unused-vars
+        const{IDLopHocDanhNghia,IDLopHocPhan,TenDot,IDDot,MaLopHocPhan,MaLopHoc,TenMonHoc,IDLopHocphan,IDSinhVien,SiSo,...rest} = dssv;
+        return rest
+      });
+    },
+    OrderProperties(data){
+      const newOrder = ["STT", "MaSinhVien", "HoDem", "Ten", "Email", "SoDienThoai", "NgaySinh2", "TenLop"];
+      const newData = data.map((item) => Object.assign({}, ...newOrder.map((key) => ({ [key]: item[key] }))))
+      console.log(newData)
+      return newData;
     },
   }
 }
